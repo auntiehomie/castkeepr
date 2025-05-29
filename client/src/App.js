30797f6022edd05249803a08bec67a22f3526014
Amanda <auntiehomie@proton.me>
2025-05-29 13:56:15 -0400

    Add complete UI component structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Menu from './components/Menu';
 import SavedCasts from './components/SavedCasts';
 import About from './components/About';
 import Donate from './components/Donate';
-import './App.css';
 
 function App() {
   return (
